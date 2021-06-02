49f9cb9bee1c4c417f4d60aa4998414b2daf48e0
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2021-06-02 13:06:45 +0530

    Added bootstrap grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserModule,AppRoutingModule,BrowserAnimationsModule,MatButtonModule,
+    MatIconModule,MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
